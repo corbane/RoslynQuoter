dacf63c2bd708688b8843aa46b1a0bcf3d0a2479
corbane <kitjmv@gmail.com>
2020-10-21 16:20:38 +0200

    Adding NodeKind control

--- src/Quoter.Eto/Quoter.Eto/MainForm.cs
+++ src/Quoter.Eto/Quoter.Eto/MainForm.cs
@@ -11,44 +11,55 @@ namespace QuoterEto
           readonly Quoter quoter;
           readonly TextArea lBox;
           readonly TextArea rBox;
-          readonly CheckToolItem openNL;
-          readonly CheckToolItem closeNL;
-          readonly CheckToolItem preserveWS;
-          readonly CheckToolItem keepApi;
-          readonly CheckToolItem staticUsing;
+          readonly CheckMenuItem openNL;
+          readonly CheckMenuItem closeNL;
+          readonly CheckMenuItem preserveWS;
+          readonly CheckMenuItem keepApi;
+          readonly CheckMenuItem staticUsing;
+          readonly ButtonMenuItem subMenu;
+          readonly RadioMenuItem kindFile;
+          readonly RadioMenuItem kindMember;
+          readonly RadioMenuItem kindStatement;
+          readonly RadioMenuItem kindExpression;
 
           public MainForm ()
           {
                Title = "My Eto Form";
-               ClientSize = new Size (400, 350);
+               ClientSize = new Size (800, 600);
 
                quoter = new Quoter ();
                lBox = new TextArea ();
                rBox = new TextArea ();
 
-               openNL = new CheckToolItem {
+               openNL = new CheckMenuItem
+               {
                     Text = "'(' + new line",
                     ToolTip = "Open parenthesis on a new line",
                };
-               closeNL = new CheckToolItem
+               closeNL = new CheckMenuItem
                {
                     Text = "new line + ')'",
                     ToolTip = "Closing parenthesis on a new line",
                };
-               preserveWS = new CheckToolItem
+               preserveWS = new CheckMenuItem
                {
                     Text = "Whitespace",
                     ToolTip = "Preserve original whitespace"
                };
-               keepApi = new CheckToolItem
+               keepApi = new CheckMenuItem
                {
                     Text = "Keep API",
-                    ToolTip = "Keep redundant API calls"
+                    ToolTip = "Keep redundant API calls",
+                    Checked = true
                };
-               staticUsing = new CheckToolItem {
+               staticUsing = new CheckMenuItem
+               {
                     Text = "using static",
-                    ToolTip = "Do require 'using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;'"
+                    ToolTip = "Do require 'using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;'",
+                    Checked = true
                };
+
+               //var nodeKind = new MenuItem ();
 
                var layout =  new DynamicLayout
                {
@@ -61,10 +72,20 @@ namespace QuoterEto
                layout.EndHorizontal();
 
                Content = layout;
-               ToolBar = new ToolBar ()
+
+               kindFile = new RadioMenuItem { Text = "File", Checked = true };
+               kindMember = new RadioMenuItem (kindFile) { Text = "Member" };
+               kindStatement = new RadioMenuItem (kindFile) { Text = "Statement" };
+               kindExpression = new RadioMenuItem (kindFile) { Text = "Expression" };
+               subMenu = new ButtonMenuItem
                {
-                    Items = { openNL, closeNL, preserveWS, keepApi, staticUsing }
+                    Text = "Parse as File",
+                    Items = { kindFile, kindMember, kindStatement, kindExpression }
                };
+
+               Menu = new MenuBar (
+                    openNL, closeNL, preserveWS, keepApi, staticUsing, new SeparatorMenuItem (), subMenu
+               );
 
                lBox.TextChanged += OnUpdate;
                openNL.CheckedChanged += OnUpdate;
@@ -72,6 +93,15 @@ namespace QuoterEto
                preserveWS.CheckedChanged += OnUpdate;
                keepApi.CheckedChanged += OnUpdate;
                staticUsing.CheckedChanged += OnUpdate;
+               kindFile.CheckedChanged += OnKindUpdate;
+               kindMember.CheckedChanged += OnKindUpdate;
+               kindStatement.CheckedChanged += OnKindUpdate;
+               kindExpression.CheckedChanged += OnKindUpdate;
+          }
+          private void OnKindUpdate (object sender, EventArgs e)
+          {
+               subMenu.Text = "Parse as " + ((RadioMenuItem)sender).Text;
+               OnUpdate (sender, e);
           }
 
           private void OnUpdate (object sender, EventArgs e)
@@ -83,8 +113,15 @@ namespace QuoterEto
                quoter.UseDefaultFormatting = !preserveWS.Checked;
                quoter.RemoveRedundantModifyingCalls = keepApi.Checked;
                quoter.ShortenCodeWithUsingStatic = staticUsing.Checked;
-               
-               var generatedNode = quoter.Quote(sourceNode.ToFullString ());
+
+               var kind
+                    = kindFile.Checked ? NodeKind.CompilationUnit
+                    : kindMember.Checked ? NodeKind.MemberDeclaration
+                    : kindStatement.Checked ? NodeKind.Statement
+                    : NodeKind.Expression;
+
+
+               var generatedNode = quoter.Quote(sourceNode.ToFullString (), kind);
                var generatedCode = quoter.Print(generatedNode);
 
                rBox.Text = generatedCode;
